41378400f950480a236c8eddceef274eeecb36c2
tiago.rossetto@logicalminds.com.br <tiago.rossetto@logicalminds.com.br>
2023-02-03 15:39:19 -0300

    Testes iniciais

---
+++ tests/MergeScripts.tests/MergeScriptsServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeScripts.tests
+{
+    public class MergeScriptsServiceTests
+    {
+        [Fact]
+        public void DeveVerificarSeDiretorioExisteComSucesso()
+        { }
+
+        [Fact]
+        public void DeveRetornarErroQuandoDiretorioNaoExistir()
+        { }
+
+        [Fact]
+        public void DeveListarOsArquivosDoDiretorioComSucesso()
+        { }
+
+        [Fact]
+        public void DeveRetornarErroQuandoNaoEncontrarArquivosNoDiretorio()
+        { }
+
+        [Fact]
+        public void DeveAbrirArquivoComSucesso()
+        { }
+
+        [Fact]
+        public void DeveCriarNovoArquivoComSucesso()
+        { }
+
+        [Fact]
+        public void DeveIncluirConteudoNoArquivoComSucesso()
+        { }
+
+        [Fact]
+        public void DeveSalvarArquivoDestinoComSucesso()
+        { }
+
+
+    }
+}
